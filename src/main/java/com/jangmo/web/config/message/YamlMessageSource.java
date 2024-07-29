c53ea2d75a4c7ae457ffb11702f3c775e6d55f52
junsang-desktop <dkwnstkdk@gmail.com>
2024-07-29 21:29:36 +0900

    YamlMessageSource bundle 파라미터 NonNull 추가

--- src/main/java/com/jangmo/web/config/message/YamlMessageSource.java
+++ src/main/java/com/jangmo/web/config/message/YamlMessageSource.java
@@ -2,14 +2,16 @@ package com.jangmo.web.config.message;
 
 import net.rakugakibox.util.YamlResourceBundle;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.lang.NonNull;
 
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class YamlMessageSource extends ResourceBundleMessageSource {
+
     @Override
-    protected ResourceBundle doGetBundle(String basename, Locale locale) throws MissingResourceException {
+    protected ResourceBundle doGetBundle(@NonNull String basename, @NonNull Locale locale) throws MissingResourceException {
         return ResourceBundle.getBundle(basename, locale, YamlResourceBundle.Control.INSTANCE);
     }
 }
