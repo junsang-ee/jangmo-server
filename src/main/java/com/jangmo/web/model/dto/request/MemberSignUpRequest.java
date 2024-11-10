813dfabbbc0cd6142f2f8fa887ab6cfdb998f40e
junsang-ee <dkwnstkddk@gmail.com>
2024-11-10 10:42:54 +0900

    파일명 변경 (MemberSignupRequest -> MemberSignUpRequest)

---
+++ src/main/java/com/jangmo/web/model/dto/request/MemberSignUpRequest.java
@@ -0,0 +1,29 @@
+package com.jangmo.web.model.dto.request;
+
+import com.jangmo.web.constants.Gender;
+import com.jangmo.web.constants.MobileCarrierType;
+import com.jangmo.web.constants.UserRole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberSignUpRequest {
+
+    private String name;
+
+    private int mobile;
+
+    private MobileCarrierType mobileCarrier;
+
+    private Gender gender;
+
+    private int birth;
+
+    private String password;
+
+    private String address;
+
+    private UserRole role;
+
+}
