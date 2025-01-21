32798e83535190a27980b62ed8120e7808e34302
junsang-desktop <dkwnstkdk@gmail.com>
2025-01-21 12:36:36 +0900

    AuthService (Mockito (단위테스트) -> SpringBootTest (통합테스트)) 로 변경

--- src/test/java/com/jangmo/web/service/AuthServiceTest.java
+++ src/test/java/com/jangmo/web/service/AuthServiceTest.java
@@ -1,82 +1,84 @@
 package com.jangmo.web.service;
 
+import com.jangmo.web.config.jwt.JwtConfig;
 import com.jangmo.web.constants.Gender;
 import com.jangmo.web.constants.MemberStatus;
+import com.jangmo.web.constants.MercenaryRetentionStatus;
 import com.jangmo.web.model.dto.request.MemberLoginRequest;
 import com.jangmo.web.model.dto.request.MemberSignUpRequest;
+import com.jangmo.web.model.dto.request.MercenaryRegistrationRequest;
 import com.jangmo.web.model.dto.response.MemberSignupResponse;
 import com.jangmo.web.model.entity.administrative.City;
 import com.jangmo.web.model.entity.administrative.District;
-import com.jangmo.web.model.entity.common.BaseUuidEntity;
 import com.jangmo.web.model.entity.user.MemberEntity;
+import com.jangmo.web.model.entity.user.MercenaryEntity;
 import com.jangmo.web.repository.CityRepository;
 import com.jangmo.web.repository.DistrictRepository;
 import com.jangmo.web.repository.MemberRepository;
-import com.jangmo.web.utils.EncryptUtil;
+import com.jangmo.web.utils.AgeUtil;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.Assertions;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.crypto.SecretKey;
-import java.lang.reflect.Field;
 import java.time.LocalDate;
-import java.util.Optional;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.any;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
-
-
 
 @Slf4j
-@ExtendWith(MockitoExtension.class)
+@RequiredArgsConstructor
+@SpringBootTest
 public class AuthServiceTest {
-    @Mock
+
+    @Autowired
     private MemberRepository memberRepository;
 
-    @Mock
+    @Autowired
     private CityRepository cityRepository;
 
-    @Mock
+    @Autowired
     private DistrictRepository districtRepository;
 
-    @InjectMocks
+    @Autowired
     private AuthServiceImpl authService;
+
+    @Autowired
+    private JwtConfig jwtConfig;
+
 
     @BeforeEach
     void init(TestInfo testInfo) {
-        if (testInfo.getDisplayName().equals("Member 회원가입 및 정보 저장 테스트")) {
-            memberRepository.deleteAll();
+        if (testInfo.getDisplayName().equals("Member 로그인 테스트")) {
+            ReflectionTestUtils.setField(
+                    jwtConfig,
+                    "secret",
+                    "dGVzdF9qc29uX3dlYl90b2tlbl9zZWNyZXRfa2V5X2Zvcl9obWFj"
+            );
         }
     }
+
 
     @DisplayName("Member 회원가입 및 정보 저장 테스트")
     @Test
     @Transactional
     void memberSignupTest() {
         LocalDate birth = LocalDate.of(1994, 3, 16);
-        City city = City.of("서울특별시");
-        District district = District.of("종로구", city);
         MemberSignUpRequest signup = new MemberSignUpRequest(
                 "testMember",
                 "01043053451",
@@ -86,28 +88,33 @@ public class AuthServiceTest {
                 1L,
                 1L
         );
-
-        given(cityRepository.findById(1L)).willReturn(Optional.of(city));
-        given(districtRepository.findById(1L)).willReturn(Optional.of(district));
-
-        MemberEntity mockMember = MemberEntity.create(signup, city, district);
-        MemberSignupResponse mockResponse = MemberSignupResponse.of(mockMember);
-
-        given(memberRepository.save(any(MemberEntity.class))).willReturn(mockMember);
+        City city = cityRepository.findById(1L).orElseThrow();
+        District district = districtRepository.findById(1L).orElseThrow();
+        MemberEntity member = MemberEntity.create(signup, city, district);
 
         MemberSignupResponse response = authService.signupMember(signup);
+        int old = AgeUtil.calculate(member.getBirth());
 
-        Assertions.assertEquals(mockResponse.getName(), response.getName());
-        Assertions.assertEquals(mockResponse.getMobile(), response.getMobile());
-        verify(memberRepository, times(1)).save(any(MemberEntity.class));
+        log.info("member name : " + member.getName() +
+                 ", member old : " + old +
+                 ", member mobile : " + member.getMobile());
+
+        log.info("response name : " + response.getName() +
+                 ", response old : " + response.getOld() +
+                 ", response mobile : " + response.getMobile());
+
+        Assertions.assertEquals(member.getName(), response.getName());
+        Assertions.assertEquals(member.getMobile(), response.getMobile());
+
+        Assertions.assertEquals(old, response.getOld());
+
     }
 
     @DisplayName("Member 로그인 테스트")
     @Test
+    @Transactional
     void memberLoginTest() {
         LocalDate birth = LocalDate.of(1994, 3, 16);
-        City city = City.of("서울특별시");
-        District district = District.of("종로구", city);
         String rawPassword = "rawPassword";
         MemberSignUpRequest signup = new MemberSignUpRequest(
                 "testMember",
@@ -118,72 +125,33 @@ public class AuthServiceTest {
                 1L,
                 1L
         );
+        City city = cityRepository.findById(signup.getCityId()).orElseThrow();
+        District district = districtRepository.findById(signup.getDistrictId()).orElseThrow();
 
-        MemberEntity mockMember = MemberEntity.create(signup, city, district);
-        try {
-            Field idField = BaseUuidEntity.class.getDeclaredField("id");
-            idField.setAccessible(true);
-            idField.set(mockMember, "a2ef3d73-27d9-480d-b245-39f518c142fe");
-        } catch (Exception ignored) {
-        }
+        authService.signupMember(signup);
+        MemberEntity signupMember = memberRepository.findByMobile("01043053451").orElseThrow();
+        signupMember.updateStatus(MemberStatus.ENABLED);
 
-        mockMember.updateStatus(MemberStatus.ENABLED);
+        MemberLoginRequest loginRequest = new MemberLoginRequest("01043053451", rawPassword);
+        String userAgent = "Mozilla/5.0 ...";
+        String token = authService.loginMember(userAgent, loginRequest);
 
-        try (MockedStatic<EncryptUtil> mockedStatic = Mockito.mockStatic(EncryptUtil.class)) {
-            mockedStatic.when(
-                    () -> EncryptUtil.matches(rawPassword, mockMember.getPassword())
-            ).thenReturn(true);
+        String secret = "dGVzdF9qc29uX3dlYl90b2tlbl9zZWNyZXRfa2V5X2Zvcl9obWFj";
+        SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
 
-            /**
-             * @TestSecretKey
-             * */
-            String secret = "dGVzdF9qc29uX3dlYl90b2tlbl9zZWNyZXRfa2V5X2Zvcl9obWFj";
-            SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        assertNotNull(token);
 
-            given(memberRepository.findByMobile("01043053451")).willReturn(Optional.of(mockMember));
+        log.info("token : " + token);
 
-            String userAgent = "Mozilla/5.0 ...";
-            MemberLoginRequest loginRequest = new MemberLoginRequest("01043053451", rawPassword);
+        String userId = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
 
-            String token = authService.loginMember(userAgent, loginRequest);
-
-            assertNotNull(token);
-
-            log.info("token : " + token);
-
-            String userId = Jwts.parser()
-                    .verifyWith(secretKey)
-                    .build()
-                    .parseSignedClaims(token)
-                    .getPayload()
-                    .getSubject();
-
-            log.info("userId : " + userId);
-
-            Assertions.assertEquals("a2ef3d73-27d9-480d-b245-39f518c142fe", userId);
-
-            verify(memberRepository, times(1)).findByMobile("01043053451");
-
-            /**
-             * Test, Dependency Issue :: AuthServiceImpl -> createJwt(); method 임의로 생성
-             * loginMember() -> 반환 메소드 임의 변경 jwtProvider.create() -> createJwt()
-             * Copy jwtProvider.create() method (AuthServiceImpl 에 임의로 변경 후 테스트)
-             *     private String createJwt(String userAgent, String id, UserRole role) {
-             *         SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode("{key}"));
-             *         Date issuedAt = new Date();
-             *         Date expireAt = new Date(issuedAt.getTime() + Duration.ofHours(2).toMillis());
-             *         return Jwts.builder()
-             *                 .subject(id)
-             *                 .claim("role", role.name())
-             *                 .claim("agent", userAgent)
-             *                 .issuedAt(issuedAt)
-             *                 .expiration(expireAt)
-             *                 .signWith(secretKey, Jwts.SIG.HS256)
-             *                 .compact();
-             *     }
-             *
-             */
-        }
+        log.info("userId : " + userId);
+        Assertions.assertEquals(signupMember.getId(), userId);
     }
 
 }
