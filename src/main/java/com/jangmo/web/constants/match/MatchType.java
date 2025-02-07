f31c8fd13e25a8ce3460c2fd8a1e7b9cf2d47fc6
junsang-ee <dkwnstkddk@gmail.com>
2025-02-07 16:18:04 +0900

    MatchType jsonCreator 추가

--- src/main/java/com/jangmo/web/constants/match/MatchType.java
+++ src/main/java/com/jangmo/web/constants/match/MatchType.java
@@ -1,5 +1,17 @@
 package com.jangmo.web.constants.match;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum MatchType {
-    REGULAR, AUTONOMOUS
+    REGULAR, AUTONOMOUS;
+
+    @JsonCreator
+    public static MatchType getType(String value) {
+        for(MatchType type : values()) {
+            if(type.name().equals(value)) {
+                return type;
+            }
+        }
+        return null;
+    }
 }
