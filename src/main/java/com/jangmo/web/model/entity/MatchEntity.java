80585bb32de755cf0a02bd653b27ceba39b59af6
junsang-ee <dkwnstkddk@gmail.com>
2025-02-11 18:54:35 +0900

    매치 투표 1:1 관계 설정 및 외래키 컬럼 추가 (In MatchEntity), static factory 생성

--- src/main/java/com/jangmo/web/model/entity/MatchEntity.java
+++ src/main/java/com/jangmo/web/model/entity/MatchEntity.java
@@ -2,25 +2,27 @@ package com.jangmo.web.model.entity;
 
 import com.jangmo.web.constants.match.MatchStatus;
 import com.jangmo.web.constants.match.MatchType;
-import com.jangmo.web.model.entity.common.CreationTimestampEntity;
-import com.jangmo.web.model.entity.user.MemberEntity;
+import com.jangmo.web.model.entity.user.UserEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+//import javax.persistence.*;
+
 import javax.persistence.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.LocalDate;
 
 import static lombok.AccessLevel.PROTECTED;
-
 
 @Getter
 @ToString
 @NoArgsConstructor(access = PROTECTED)
 @Entity(name = "match")
 public class MatchEntity extends CreationUserEntity {
+
+    @Column(nullable = false)
+    private LocalDate matchAt;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -30,12 +32,21 @@ public class MatchEntity extends CreationUserEntity {
     @Enumerated(EnumType.STRING)
     private MatchType type;
 
-//    @OneToMany(mappedBy = "match", cascade = CascadeType.REMOVE)
-//    private List<MatchUserEntity> players;
-
-    @OneToOne(mappedBy = "match", fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "match_vote")
     private MatchVoteEntity matchVote;
 
+    private MatchEntity(UserEntity user, LocalDate matchAt, MatchType type) {
+        super(user);
+        this.matchAt = matchAt;
+        this.type = type;
+        this.status = MatchStatus.PENDING;
+    }
 
+    public static MatchEntity create(final UserEntity user,
+                                     final LocalDate matchAt,
+                                     final MatchType type) {
+        return new MatchEntity(user, matchAt, type);
+    }
 }
 
