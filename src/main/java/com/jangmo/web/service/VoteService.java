956371e5ca838979f48cfa330d4e50e3d7ddffce
junsang-ee <dkwnstkddk@gmail.com>
2025-02-12 19:11:44 +0900

    패키지 변경으로 파일 삭제(파일 삭제 후 새로운 파일 생성으로 이동) VoteService UserManagementService

--- src/main/java/com/jangmo/web/service/VoteService.java
+++
@@ -1,9 +0,0 @@
-package com.jangmo.web.service;
-
-import com.jangmo.web.model.dto.request.MatchVoteCreateRequest;
-import com.jangmo.web.model.entity.MatchVoteEntity;
-import com.jangmo.web.model.entity.user.UserEntity;
-
-public interface VoteService {
-    MatchVoteEntity createMatchVote(UserEntity writer, MatchVoteCreateRequest request);
-}
--- src/main/java/com/jangmo/web/service/management/UserManagementService.java
+++
@@ -1,7 +0,0 @@
-package com.jangmo.web.service.management;
-
-public interface UserManagementService {
-    void approveMercenary(String mercenaryId);
-
-    void approveMember(String memberId);
-}
