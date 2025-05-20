1ad29f4fab542985204f936c17c5d673f9cf6adb
junsang-ee <dkwnstkddk@gmail.com>
2025-05-20 19:56:40 +0900

    패키지 이동으로 파일 삭제 처리 (exception/custom/ exceptions...)

--- src/main/java/com/jangmo/web/exception/custom/AuthException.java
+++
@@ -1,9 +0,0 @@
-package com.jangmo.web.exception.custom;
-
-import com.jangmo.web.constants.message.ErrorMessage;
-
-public class AuthException extends BaseException {
-    public AuthException(ErrorMessage error) {
-        super(error);
-    }
-}
--- src/main/java/com/jangmo/web/exception/custom/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.jangmo.web.exception.custom;
-
-import com.jangmo.web.constants.message.ErrorMessage;
-
-public class NotFoundException extends BaseException {
-    public NotFoundException(ErrorMessage error) {
-        super(error);
-    }
-}
