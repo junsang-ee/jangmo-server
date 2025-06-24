8b741649c9a81b860da21a18899ab521ab43dde8
junsang-ee <dkwnstkddk@gmail.com>
2025-06-24 20:56:16 +0900

    Ground, User, Vote management Controller 생성 (기존 ManagementController 에 취합해놓은 내용 분리)

--- src/main/java/com/jangmo/web/controller/manager/UserManagementController.java
+++ src/main/java/com/jangmo/web/controller/manager/UserManagementController.java
@@ -2,60 +2,43 @@ package com.jangmo.web.controller.manager;
 
 import com.jangmo.web.constants.user.MemberStatus;
 import com.jangmo.web.constants.user.MercenaryStatus;
-import com.jangmo.web.model.dto.request.GroundCreateRequest;
-import com.jangmo.web.model.dto.request.MatchVoteCreateRequest;
 import com.jangmo.web.model.dto.request.MercenaryMatchRequest;
 import com.jangmo.web.model.dto.request.UserListSearchRequest;
-
-import com.jangmo.web.model.dto.response.MatchVoteCreateResponse;
 import com.jangmo.web.model.dto.response.MemberDetailResponse;
 import com.jangmo.web.model.dto.response.MercenaryDetailResponse;
 import com.jangmo.web.model.dto.response.UserListResponse;
-import com.jangmo.web.model.dto.response.SearchPlaceResponse;
-import com.jangmo.web.model.dto.response.GroundCreateResponse;
 import com.jangmo.web.model.dto.response.common.ApiSuccessResponse;
-
 import com.jangmo.web.model.dto.response.common.PageResponse;
 import com.jangmo.web.model.entity.user.UserEntity;
-import com.jangmo.web.service.manager.GroundManagementService;
 import com.jangmo.web.service.manager.UserManagementService;
-import com.jangmo.web.service.manager.VoteService;
-
 import lombok.RequiredArgsConstructor;
-
-import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import javax.validation.Valid;
 import java.util.List;
 
-import static com.jangmo.web.model.dto.response.common.ApiSuccessResponse.wrap;
 import static com.jangmo.web.model.dto.response.common.ApiSuccessResponse.page;
+import static com.jangmo.web.model.dto.response.common.ApiSuccessResponse.wrap;
 
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/managers")
 @RestController
-public class ManagerController {
-
-    private final VoteService voteService;
-
+public class UserManagementController {
+    
     private final UserManagementService userManagementService;
 
-    private final GroundManagementService groundManagementService;
+    @GetMapping("/users")
+    public ResponseEntity<ApiSuccessResponse<PageResponse<UserListResponse>>> getUserList(
+            @AuthenticationPrincipal(expression = "id") String myId,
+            @ParameterObject UserListSearchRequest request,
+            Pageable pageable) {
+        return page(userManagementService.getUserList(myId, request, pageable));
+    }
+
+
     @GetMapping("/members/{memberId}")
     public ResponseEntity<ApiSuccessResponse<MemberDetailResponse>> getMemberDetail(@PathVariable String memberId) {
         return wrap(userManagementService.getMemberDetail(memberId));
@@ -64,14 +47,6 @@ public class ManagerController {
     @GetMapping("/mercenaries/{mercenaryId}")
     public ResponseEntity<ApiSuccessResponse<MercenaryDetailResponse>> getMercenaryDetail(@PathVariable String mercenaryId) {
         return wrap(userManagementService.getMercenaryDetail(mercenaryId));
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<ApiSuccessResponse<PageResponse<UserListResponse>>> getUserList(
-            @AuthenticationPrincipal(expression = "id") String myId,
-            @ParameterObject UserListSearchRequest request,
-            Pageable pageable) {
-        return page(userManagementService.getUserList(myId, request, pageable));
     }
 
     @PatchMapping("/mercenaries/{mercenaryId}/approve")
@@ -102,33 +77,9 @@ public class ManagerController {
         return wrap(null);
     }
 
-    @PostMapping("/votes/matches")
-    public ResponseEntity<ApiSuccessResponse<MatchVoteCreateResponse>> createMatchVote(
-            @AuthenticationPrincipal(expression = "id") String userId,
-            @Valid @RequestBody MatchVoteCreateRequest request) {
-        return wrap(voteService.createMatchVote(userId, request));
-    }
-
-    @DeleteMapping("/votes/{voteId}")
-    public ResponseEntity<ApiSuccessResponse<Object>> deleteVote(@PathVariable String voteId) {
-        return wrap(null);
-    }
-
     @GetMapping("/users/pending-approval")
     public ResponseEntity<ApiSuccessResponse<List<UserEntity>>> getApprovalUsers() {
         return wrap(userManagementService.getApprovalUsers());
-    }
-
-    @GetMapping("/ground/{keyword}")
-    public ResponseEntity<ApiSuccessResponse<List<SearchPlaceResponse>>> searchGrounds(@PathVariable String keyword) {
-        return wrap(groundManagementService.searchGrounds(keyword));
-    }
-
-    @PostMapping("/ground")
-    public ResponseEntity<ApiSuccessResponse<GroundCreateResponse>> createGround(
-            @AuthenticationPrincipal(expression = "id") String createdById,
-            @RequestBody GroundCreateRequest request) {
-        return wrap(groundManagementService.createGround(createdById, request));
     }
 
 
