aaa35c27e463cc5883d50475030fb20c1f72e9ac
junsang-ee <dkwnstkddk@gmail.com>
2025-07-17 21:15:33 +0900

    dto 명 변경 (naming 통일) MemberUpdateAddressRequest -> MemberAddressUpdateRequest, MemberUpdatePasswordRequest -> MemberPasswordUpdateRequest ErrorMessage 추가 (Member role 변경 시 이미 동일한 권한일 때의 에러)

---
+++ src/main/java/com/jangmo/web/model/dto/request/MemberAddressUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.jangmo.web.model.dto.request;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class MemberAddressUpdateRequest {
+
+    @NotNull(message = "시/도는 필수 항목입니다.")
+    private Long cityId;
+
+    @NotNull(message = "시/군/구는 필수 항목입니다.")
+    private Long districtId;
+
+}
---
+++ src/main/java/com/jangmo/web/model/dto/request/MemberPasswordUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.jangmo.web.model.dto.request;
+
+import com.jangmo.web.config.validator.ValidFields;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class MemberPasswordUpdateRequest {
+
+    @NotBlank(message = "현재 비밀번호는 필수 항목입니다.")
+    @ValidFields(field = "password")
+    private String oldPassword;
+
+    @NotBlank(message = "새로운 비밀번호는 필수 항목입니다.")
+    @ValidFields(field = "password")
+    private String newPassword;
+}
