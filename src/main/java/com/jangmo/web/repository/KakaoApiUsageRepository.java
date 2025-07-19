7f895f7cdf374dc812f73ea9eaba6e008ac381e4
junsang-ee <dkwnstkddk@gmail.com>
2025-07-19 20:16:56 +0900

    UserRole - DIRECTOR (감독) 권한 삭제

---
+++ src/main/java/com/jangmo/web/repository/KakaoApiUsageRepository.java
@@ -0,0 +1,15 @@
+package com.jangmo.web.repository;
+
+import com.jangmo.web.model.entity.api.KakaoApiUsageEntity;
+import com.jangmo.web.model.entity.user.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface KakaoApiUsageRepository extends JpaRepository<KakaoApiUsageEntity, String> {
+    Optional<KakaoApiUsageEntity> findByApiCaller(MemberEntity apiCaller);
+
+    void deleteByApiCaller(MemberEntity apiCaller);
+}
