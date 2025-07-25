da7fb2bfab40417bf600682213294c56fe3a14d4
junsang-ee <dkwnstkddk@gmail.com>
2025-07-25 19:02:13 +0900

    ConflictStateException 추가

---
+++ src/main/java/com/jangmo/web/exception/conflict/ConflictStateException.java
@@ -0,0 +1,10 @@
+package com.jangmo.web.exception.conflict;
+
+import com.jangmo.web.constants.message.ErrorMessage;
+
+public class ConflictStateException extends ConflictException {
+
+    public ConflictStateException(ErrorMessage error) {
+        super(error);
+    }
+}
