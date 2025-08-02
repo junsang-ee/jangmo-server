a5a5aa96f83b1e5c4652fd0fa98ab429323106fe
junsang-ee <dkwnstkddk@gmail.com>
2025-08-03 04:50:07 +0900

    PostController, PostService 게시글 관련 아키택쳐 파일 생성 findByParentBoard ( 부모 게시판에 의한 post list 메소드 추가 ( in postRepository)) PostList 게시글 리스트 API 및 서비스 생성 (In BoardController -> PostService) PostList Response DTO 추가 (post list 응답 dto) Post 상세 API 및 로직 추가

--- src/main/java/com/jangmo/web/controller/board/PostController.java
+++ src/main/java/com/jangmo/web/controller/board/PostController.java
@@ -1,9 +1,7 @@
 package com.jangmo.web.controller.board;
 
-import com.jangmo.web.model.dto.response.board.BoardListResponse;
-import com.jangmo.web.model.dto.response.board.PostListResponse;
 import com.jangmo.web.model.dto.response.common.ApiSuccessResponse;
-import com.jangmo.web.service.board.BoardService;
+import com.jangmo.web.model.entity.board.PostEntity;
 import com.jangmo.web.service.board.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,28 +10,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 import static com.jangmo.web.model.dto.response.common.ApiSuccessResponse.wrap;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/boards")
+@RequestMapping("/api/posts")
 @RestController
-public class BoardController {
-
-    private final BoardService boardService;
+public class PostController {
 
     private final PostService postService;
 
-    @GetMapping
-    public ResponseEntity<ApiSuccessResponse<List<BoardListResponse>>> list() {
-        return wrap(boardService.list());
-    }
-
-    @GetMapping("/{boardId}/posts")
-    public ResponseEntity<ApiSuccessResponse<List<PostListResponse>>> postList(
-            @PathVariable String boardId
+    @GetMapping("/{postId}")
+    public ResponseEntity<ApiSuccessResponse<PostEntity>> list(
+            @PathVariable String postId
     ) {
-        return wrap(postService.list(boardId));
+        return wrap(postService.detail(postId));
     }
 }
