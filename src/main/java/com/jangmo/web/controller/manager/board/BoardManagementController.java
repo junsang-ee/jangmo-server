3d626a8aac3689f393a6e28b04c86e139829fabc
junsang-ee <dkwnstkddk@gmail.com>
2025-08-07 18:26:21 +0900

    게시판 매니저 API RequestMapping Url 변경 (In PostManagementController /api/managers/boards -> /api/managers/posts) 기존 게시판 생성 api 이동 (PostManagementController -> BoardManagementController)

--- src/main/java/com/jangmo/web/controller/manager/board/BoardManagementController.java
+++ src/main/java/com/jangmo/web/controller/manager/board/BoardManagementController.java
@@ -2,11 +2,15 @@ package com.jangmo.web.controller.manager.board;
 
 import com.jangmo.web.model.dto.request.board.manager.BoardCreateRequest;
 import com.jangmo.web.model.dto.request.board.manager.BoardUpdateRequest;
+import com.jangmo.web.model.dto.request.board.manager.PostCreateRequest;
 import com.jangmo.web.model.dto.response.board.manager.BoardCreateResponse;
+import com.jangmo.web.model.dto.response.board.manager.PostCreateResponse;
 import com.jangmo.web.model.dto.response.common.ApiSuccessResponse;
 import com.jangmo.web.service.manager.board.BoardManagementService;
+import com.jangmo.web.service.manager.board.PostManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,6 +23,8 @@ import static com.jangmo.web.model.dto.response.common.ApiSuccessResponse.wrap;
 public class BoardManagementController {
 
     private final BoardManagementService boardManagementService;
+
+    private final PostManagementService postManagementService;
 
     @PostMapping
     public ResponseEntity<ApiSuccessResponse<BoardCreateResponse>> create(
@@ -33,5 +39,14 @@ public class BoardManagementController {
         boardManagementService.update(boardId, request);
         return wrap(null);
     }
+
+    @PostMapping("/{boardId}/posts")
+    public ResponseEntity<ApiSuccessResponse<PostCreateResponse>> createPost(
+            @AuthenticationPrincipal(expression = "id") String memberId,
+            @PathVariable String boardId,
+            @Valid @RequestBody PostCreateRequest request) {
+        return wrap(postManagementService.create(memberId, boardId, request));
+    }
+
 
 }
