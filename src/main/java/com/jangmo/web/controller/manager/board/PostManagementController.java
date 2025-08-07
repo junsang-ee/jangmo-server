307f232b8dc307905bc2737d3770e7e62686f031
junsang-ee <dkwnstkddk@gmail.com>
2025-08-07 18:26:21 +0900

    게시판 매니저 API RequestMapping Url 변경 (In PostManagementController /api/managers/boards -> /api/managers/posts) 기존 게시판 생성 api 이동 (PostManagementController -> BoardManagementController)

--- src/main/java/com/jangmo/web/controller/manager/board/PostManagementController.java
+++ src/main/java/com/jangmo/web/controller/manager/board/PostManagementController.java
@@ -1,14 +1,10 @@
 package com.jangmo.web.controller.manager.board;
 
-
-import com.jangmo.web.model.dto.request.board.manager.PostCreateRequest;
 import com.jangmo.web.model.dto.request.board.manager.PostUpdateRequest;
-import com.jangmo.web.model.dto.response.board.manager.PostCreateResponse;
 import com.jangmo.web.model.dto.response.common.ApiSuccessResponse;
 import com.jangmo.web.service.manager.board.PostManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,19 +12,11 @@ import javax.validation.Valid;
 import static com.jangmo.web.model.dto.response.common.ApiSuccessResponse.wrap;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/managers/boards")
+@RequestMapping("/api/managers/posts")
 @RestController
 public class PostManagementController {
 
     private final PostManagementService postManagementService;
-
-    @PostMapping("/{boardId}/posts")
-    public ResponseEntity<ApiSuccessResponse<PostCreateResponse>> create(
-            @AuthenticationPrincipal(expression = "id") String memberId,
-            @PathVariable String boardId,
-            @Valid @RequestBody PostCreateRequest request) {
-        return wrap(postManagementService.create(memberId, boardId, request));
-    }
 
     @PatchMapping("/{postId}")
     public ResponseEntity<ApiSuccessResponse<Object>> update(
@@ -37,5 +25,7 @@ public class PostManagementController {
         postManagementService.update(postId, request);
         return wrap(null);
     }
+
+
 
 }
