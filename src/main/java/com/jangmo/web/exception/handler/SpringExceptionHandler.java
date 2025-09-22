18774896487bb600ba7b29399a44beac64b3bb8b
junsang-ee <dkwnstkddk@gmail.com>
2025-09-23 02:30:54 +0900

    BindException 추가 (SpringExceptionHandler, ExceptionHandler), (field 타입 에러 등)

--- src/main/java/com/jangmo/web/exception/handler/SpringExceptionHandler.java
+++ src/main/java/com/jangmo/web/exception/handler/SpringExceptionHandler.java
@@ -4,13 +4,16 @@ import com.jangmo.web.constants.message.ErrorMessage;
 import com.jangmo.web.exception.handler.base.BaseExceptionHandler;
 import com.jangmo.web.model.dto.response.common.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+
 import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataAccessResourceFailureException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -91,6 +94,33 @@ public class SpringExceptionHandler extends BaseExceptionHandler {
         log.debug("Exception stack", ex);
 
         return toResponse(ErrorMessage.REQUEST_BODY_FIELD, new String[] {message});
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ApiErrorResponse> handleBindException(BindException ex) {
+        BindingResult result = ex.getBindingResult();
+        FieldError fieldError = result.getFieldError();
+        String field = Objects.requireNonNull(fieldError).getField();
+        String message = fieldError.getDefaultMessage();
+
+        if (Objects.equals(fieldError.getCode(), "typeMismatch")) {
+            String koField = getFieldToKorean(field);
+            message = "{" + koField + "} 값이 올바른 형식이 아닙니다.";
+        }
+
+        log.warn("RequestParamValidationError >> field={}, message={}", field, message);
+        log.debug("Exception stack", ex);
+
+        return toResponse(ErrorMessage.REQUEST_QUERY_PARAM, new String[] {message});
+    }
+
+    private String getFieldToKorean(String field) {
+        switch (field) {
+            case "year": return "년도";
+            case "month": return "월";
+            case "voteType": return "투표 타입";
+            default: return field;
+        }
     }
 
 
