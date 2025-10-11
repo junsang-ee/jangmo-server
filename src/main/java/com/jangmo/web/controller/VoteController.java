5e6568cb4ad5a045291beab97903b47a06019584
junsang-ee <dkwnstkddk@gmail.com>
2025-10-12 03:16:46 +0900

    - VoteController 추가 및 투표 API url, method 추가 	- 투표 API Post method 추가 (castMatchVote())

---
+++ src/main/java/com/jangmo/web/controller/VoteController.java
@@ -0,0 +1,29 @@
+package com.jangmo.web.controller;
+
+
+import com.jangmo.web.model.dto.request.vote.MatchVoteCastRequest;
+import com.jangmo.web.model.dto.response.common.ApiSuccessResponse;
+import com.jangmo.web.service.VoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import static com.jangmo.web.model.dto.response.common.ApiSuccessResponse.wrap;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/votes")
+@RestController
+public class VoteController {
+
+    private final VoteService voteService;
+
+    @PostMapping("/{matchVoteId}")
+    public ResponseEntity<ApiSuccessResponse<Object>> castMatchVote(
+            @PathVariable String matchVoteId,
+            @AuthenticationPrincipal(expression = "id") String userId,
+            @RequestBody MatchVoteCastRequest request) {
+        voteService.castMatchVote(matchVoteId, userId, request);
+        return wrap(null);
+    }
+}
