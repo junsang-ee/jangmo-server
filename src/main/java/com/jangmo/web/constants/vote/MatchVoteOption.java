762f332063690d6f5d9db4494ad919f21fefa523
junsang-ee <dkwnstkddk@gmail.com>
2025-10-12 03:08:28 +0900

    - 요청 값 validation custom annotation 메소드화 및 Enum Validation 추가 (MatchVoteOption) - ErrorMessage 추가 및 일부 변경(에 따른 messages_ko.yml 내용 추가) 	- 추가 		- MATCH_VOTE_INVALID_OPTION (매치 투표 시 미투표 상태값 요청 예외) 		- MATCH_VOTE_USER_NOT_FOUND (매치 투표 시 유권자 목록 찾을 수 없음 예외) 	- 변경 		- MATCH_VOTE_NOT_FOUND (5021 -> 5030) - MatchVoteOption enum -> JsonCreator 추가 ( null 반환 메소드)

--- src/main/java/com/jangmo/web/constants/vote/MatchVoteOption.java
+++ src/main/java/com/jangmo/web/constants/vote/MatchVoteOption.java
@@ -1,9 +1,17 @@
 package com.jangmo.web.constants.vote;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum MatchVoteOption {
-    NOT_VOTED,
-    ANTE_MERIDIEM,
-    POST_MERIDIEM,
-    ABSENT,
-    PENDING
+    NOT_VOTED, ANTE_MERIDIEM, POST_MERIDIEM, ABSENT, PENDING;
+
+    @JsonCreator
+    public static MatchVoteOption getValue(String value) {
+        for (MatchVoteOption option : values()) {
+            if (option.name().equals(value)) {
+                return option;
+            }
+        }
+        return null;
+    }
 }
