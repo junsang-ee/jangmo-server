97272991b36856bb50af6ea95dee1b50ea49f0cc
junsang-ee <dkwnstkddk@gmail.com>
2025-10-24 17:01:54 +0900

    - SmsConfig 주입 부분 일괄 SmsProperties 로 수정 (SmsProvider, SmsProviderTest) - 쓰이지 않는 의존 주입 모듈 제거

--- src/main/java/com/jangmo/web/config/admin/AdminInitializationConfig.java
+++ src/main/java/com/jangmo/web/config/admin/AdminInitializationConfig.java
@@ -16,7 +16,6 @@ import com.jangmo.web.repository.user.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
