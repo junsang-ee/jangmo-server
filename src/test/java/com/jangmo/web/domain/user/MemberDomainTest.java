2431755b9e0682f4606e6224f217e9a967e6ffc6
junsang-ee <dkwnstkddk@gmail.com>
2026-01-07 17:53:45 +0900

    feat: 회원 domain test 추가

---
+++ src/test/java/com/jangmo/web/domain/user/MemberDomainTest.java
@@ -0,0 +1,10 @@
+package com.jangmo.web.domain.user;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@ActiveProfiles("test")
+public class MemberDomainTest {
+}
